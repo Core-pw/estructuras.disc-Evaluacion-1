8a77b337d7b5a9fc991afb37d0309f1b7fedfe32
Core <anderson.pwrb@gmail.com>
2020-08-13 12:29:16 -0400

    detecta si el usuario introdujo 'a' o 'b' en la expresion, se introdujo un historial y hubieron cambios menores en app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('form');
-const conjunto = document.getElementById('conjunto');
+const conjunto = document.querySelector('select');
 const respuesta = document.querySelector('p');
+const historial = document.querySelector('p.historial');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
--- expresion.js
+++ expresion.js
@@ -4,12 +4,11 @@ class Expresion{
         this.conjunto = conjunto;
     }
 
-    getInput(){
-        return new Function(`return ${this.input}`)();
-    }
+    static evaluations = 0;
 
     evaluate(){
         try {
+            this.input = this.detectVar();
             const operacion = this.getInput();
 
             if(operacion !== undefined){
@@ -51,12 +50,127 @@ class Expresion{
                     default:
                         respuesta.innerHTML += 'Por favor escoja un conjunto';
                 }
+                this.updateHistory(operacion);
                 form.reset();
     
-            }else respuesta.innerHTML = "Por favor no deje vacío el campo de operación";
+            }else respuesta.innerHTML = "Por favor no deje vacío el campo de operación<br>ni introduzca caracteres no aceptados";
            
         } catch {
             respuesta.innerHTML = "Por favor introduzca una expresión valida";
         }
     }
+
+    detectVar(){
+        if(!this.input.includes('Math') && this.input.indexOf('a') >= 0 && this.input.indexOf('b') >= 0)
+            return this.twoVar();
+
+        else if(!this.input.includes('Math') && this.input.indexOf('a') >= 0 || this.input.indexOf('b') >= 0)
+            return this.oneVar();
+
+        else return this.input;
+    }
+
+    oneVar(){
+        let signo = Math.random() < 0.50 ? -1 : 1;
+        let number;
+
+        switch (this.conjunto) {
+            case 'naturales':
+                number = this.randomNumber(1, 99);
+                break;
+
+            case 'enteros':
+                number = signo*this.randomNumber(0, 99);
+                break;
+            
+            case 'racionales':
+                number = signo*this.randomNumber(0, 99);
+                break;
+            
+            case 'irracionales':
+                number = Math.sqrt(-1*this.randomNumber(1, 99));
+                break;
+            
+            case 'reales':
+                number = signo*this.randomNumber(0, 99);          
+        }
+       return this.insertOneNumber(number.toString());
+    }
+
+    twoVar(){
+        let signo = Math.random() < 0.50 ? -1 : 1;
+        let firstNumber;
+        let secondNumber;
+
+        switch (this.conjunto) {
+            case 'naturales':
+                firstNumber = this.randomNumber(1, 99);
+                secondNumber = this.randomNumber(1, 99);
+                break;
+
+            case 'enteros':
+                firstNumber = signo*this.randomNumber(0, 99);
+                secondNumber = signo*this.randomNumber(0, 99);
+                break;
+            
+            case 'racionales':
+                firstNumber = signo*this.randomNumber(0, 99);
+                secondNumber = signo*this.randomNumber(0, 99);
+                break;
+            
+            case 'irracionales':
+                firstNumber = Math.sqrt(-1*this.randomNumber(1, 99));
+                secondNumber = Math.sqrt(-1*this.randomNumber(1, 99));
+                break;
+            
+            case 'reales':
+                firstNumber = signo*this.randomNumber(0, 99);
+                secondNumber = signo*this.randomNumber(0, 99);           
+        }
+       return this.insertTwoNumbers(firstNumber.toString(), secondNumber.toString());
+    }
+
+    randomNumber(min, max){
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    insertOneNumber(number){
+        const arrayInput = [...this.input];
+
+        arrayInput.forEach((element, index) => {
+            if(element === 'a' || element === 'b')
+                arrayInput[index] = number;
+        });
+        console.log(arrayInput.join(''));
+        return arrayInput.join('');
+    }
+
+    insertTwoNumbers(firstNumber, secondNumber){
+        const arrayInput = [...this.input];
+
+        arrayInput.forEach((element, index) => {
+            if(element === 'a' || element === 'b'){
+                if(element === 'a')
+                    arrayInput[index] = firstNumber;
+                else arrayInput[index] = secondNumber;                   
+            }           
+        });
+        console.log(arrayInput.join(''));
+        return arrayInput.join('');
+    }
+
+    getInput(){
+        return new Function(`return ${this.input}`)();
+    }
+
+    updateHistory(operacion){
+        historial.innerHTML += `${this.input}= ${operacion}<br>`; 
+        Expresion.evaluations++;
+        
+        if(Expresion.evaluations > 3){
+            historial.innerHTML = '';
+            historial.innerHTML += `${this.input}= ${operacion}<br>`; 
+        }
+    }
+
 }
